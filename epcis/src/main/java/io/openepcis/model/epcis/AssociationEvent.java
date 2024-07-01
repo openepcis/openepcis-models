873b87fc7f4b48855d96e0f69d4476da60a6bc32
Aravinda Baliga B <aravinda.baliga@benelog.com>
2024-07-01 17:54:39 +0200

    EDG-33: convert the certificationInfo field from string to object. accordingly fix associated modules and examples.

--- epcis/src/main/java/io/openepcis/model/epcis/AssociationEvent.java
+++ epcis/src/main/java/io/openepcis/model/epcis/AssociationEvent.java
@@ -125,7 +125,7 @@ public class AssociationEvent extends EPCISEvent implements XmlSupportExtension 
       Map<String, Object> userExtensions,
       Map<String, Object> innerUserExtensions,
       List<Object> contextInfo,
-      String certificationInfo,
+      Map<String, Object> certificationInfo,
       List<SourceList> sourceList,
       List<DestinationList> destinationList,
       List<SensorElementList> sensorElementList,
